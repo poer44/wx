edde116e13904fd45ad2516af7ce5e6746ff0bdc
poer44 <poer44@outlook.com>
2019-06-18 21:43:38 +0800

    Initial commit

---
+++ src/main/java/com/poer/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.poer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping("/test")
+    public String video(){
+        return "./test.mp4";
+    }
+}
