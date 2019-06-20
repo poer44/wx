563c282ccb14ccd1e2b49fd182aa249fba99a4c0
xuebw <Poer666>
2019-06-20 15:04:34 +0800

    提交demo图片

--- src/main/java/com/poer/demo/DemoApplication.java
+++ src/main/java/com/poer/demo/DemoApplication.java
@@ -2,8 +2,17 @@ package com.poer.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @SpringBootApplication
 @Controller
@@ -13,8 +22,4 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @RequestMapping("/test")
-    public String video(){
-        return "./test.mp4";
-    }
 }
